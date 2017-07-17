ec0bc1d2b1ea37b0b506bb3f00032732f576eec8
josh <joshualim.1@gmail.com>
2017-07-16 21:34:05 -0700

    url mapping fixed

--- ortus/api/urls.py
+++ ortus/api/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import include, url
 from rest_framework import routers
-
+from api import viewsets
 # Routers define the URL mapping to views
 router = routers.DefaultRouter()
 # router.register(r'users', UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls, namespace='rest_api')),
+    url(r'^members/$', viewsets.member_list),
+    url(r'^members/(?P<pk>[0-9]+)/$', viewsets.member_detail),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
