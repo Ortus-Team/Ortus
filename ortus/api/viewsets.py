5464a41dff3157415e6ae9e141640884c49af4a8
josh <joshualim.1@gmail.com>
2017-07-17 22:11:38 -0700

    adding view endpoint

--- ortus/api/viewsets.py
+++ ortus/api/viewsets.py
@@ -2,9 +2,8 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
-from main.models import User
-from api.serializers import UserSerializer
-from api.serializers import SignUpSerializer
+from main.models import *
+from api.serializers import *
 from rest_framework import generics
 from api.permissions import IsAuthenticatedOrCreate
 # from rest_framework import status
@@ -54,6 +53,74 @@ def member_detail(request, pk):
 	elif request.method == 'DELETE':
 		user.delete()
 		return HttpResponse(status=204)
+
+@csrf_exempt
+def event_detail(request, pk):
+	try:
+		event = Event.objects.get(pk=pk)
+	except Event.DoesNotExist:
+		return HttpResponse(status=404)
+
+	if request.method == 'GET':
+		serializer = EventSerializer(event)
+		return JsonResponse(serializer.data)
+	elif request.method == 'PUT':
+		data = JSONParser().parse(request)
+		serializer = EventSerializer(event, data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data)
+		return JsonResponse(serializer.errors, status=404)
+
+@csrf_exempt
+def org_detail(request, pk):
+	try:
+		org = Org.objects.get(pk=pk)
+	except Org.DoesNotExist:
+		return HttpResponse(status=404)
+	if request.method == 'GET':
+		serializer = OrgSerializer(org)
+		return JsonResponse(serializer.data)
+	elif request.method == 'PUT':
+		data = JSONParser().parse(request)
+		serializer = OrgSerializer(org, data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data)
+		return JsonResponse(serializer.errors, status=404)
+
+@csrf_exempt
+def post_detail(request, pk):
+	try:
+		post = Post.objects.get(pk=pk)
+	except Post.DoesNotExist:
+		return HttpResponse(status=404)
+	if request.method == 'GET':
+		serializer = PostSerializer(post)
+		return JsonResponse(serializer.data)
+	elif request.method == 'PUT':
+		data = JSONParser().parse(request)
+		serializer = PostSerializer(post, data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data)
+		return JsonResponse(serializer.errors, status=404)
+
+@csrf_exempt
+def photo_detail(request, pk):
+	try:
+		photo = Photo.objects.get(pk=pk)
+	except Photo.DoesNotExist:
+		return HttpResponse(status=404)
+	if request.method == 'GET':
+		serializer = PhotoSerializer(photo)
+		return JsonResponse(serializer.data)
+	elif request.method == 'PUT':
+		serializer = PhotoSerializer(photo, data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data)
+		return JsonResponse(serializer.errors, status=404)	
 
 class SignUp(generics.CreateAPIView):
 	queryset = User.objects.all()
