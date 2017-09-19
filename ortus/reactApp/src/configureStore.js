1a3c09b08f958b029531335805c62e72cd2aef44
Ahhyun Cho <ahhyun01@cs.washington.edu>
2017-09-18 20:30:21 -0700

    Set up redux

---
+++ ortus/reactApp/src/configureStore.js
@@ -0,0 +1,19 @@
+import createLogger from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+
+import rootReducer from "./rootReducer";
+
+const loggerMiddleware = createLogger({
+  collapsed: true,
+});
+
+
+export default initialState => createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware,
+  ),
+);
